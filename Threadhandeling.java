6edc30be6fad7c7f8b001699376a0b17274363e1
RITAM CHARAN <ritamcharan232001@gmail.com>
2021-10-12 23:08:44 +0530

    Update Threadhandeling.java

--- Threadhandeling.java
+++ Threadhandeling.java
@@ -1,1 +1,22 @@
 //your code here
+class Multi3 implements Runnable{  
+public void run(){  
+System.out.println("thread is running...");  
+}  
+  
+public static void main(String args[]){  
+Multi3 m1=new Multi3();  
+Thread t1 =new Thread(m1);  
+t1.start();  
+ }  
+}  
+
+class Multi extends Thread{  
+public void run(){  
+System.out.println("thread is running...");  
+}  
+public static void main(String args[]){  
+Multi t1=new Multi();  
+t1.start();  
+ }  
+}  
